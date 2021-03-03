e7736628d1dce4734089965ac78e1dfe7d6a9328
Santiago <santiago.sanchez@dinotech.com>
2021-03-03 19:45:04 +0100

    Added create order

---
+++ src/test/scala/com/crypto/SigProviderSpec.scala
@@ -0,0 +1,42 @@
+package com.crypto
+
+import cats.implicits.catsSyntaxOptionId
+import com.crypto.request.{CreateOrderRequestParams, CroPrivateRequest}
+import faith.knowledge.common.ChainingSyntax.scalaUtilChainingOps
+import io.circe.syntax.EncoderOps
+import org.apache.commons.codec.binary.Hex
+import org.apache.commons.codec.digest.{HmacAlgorithms, HmacUtils}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.util.chaining.scalaUtilChainingOps
+
+class SigProviderSpec extends AnyWordSpec with Matchers {
+  "A sig provider with a given key and pass" should {
+    val apiKey      = "apiKey"
+    val secretKey   = "secretKey"
+    val sigProvider = new SigProvider(apiKey, secretKey)
+    "encode properly a given request" in {
+      val instrumentName       = "BTC_CRO"
+      val side                 = "SELL"
+      val `type`               = "MARKET"
+      val notional: BigDecimal = 10
+      val method               = "private/create-order"
+      val id                   = 20
+      val nonce                = 203
+      val request = CreateOrderRequestParams(
+        instrumentName = instrumentName,
+        notional = notional.some,
+        side = side,
+        `type` = `type`
+      ) pipe (params => CroPrivateRequest(id = id, method = method, nonce = nonce, params = params.some))
+      val noHashSig = s"$method$id${apiKey}instrument_name${instrumentName}notional${notional}side${side}type${`type`}$nonce"
+      val expectedSig = (
+        noHashSig
+          |> new HmacUtils(HmacAlgorithms.HMAC_SHA_256, secretKey).hmac
+          |> Hex.encodeHexString
+      )
+      sigProvider.signRequest(request).sig shouldBe expectedSig
+    }
+  }
+}
